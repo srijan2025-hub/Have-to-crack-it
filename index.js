a7a069efd31f449a491a91ce4e103c7597d9edd2
srijan2025-hub <srijangithub@gmail.com>
2026-01-23 19:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const body = document.body;
 toggleBtn.addEventListener("click", () => {
   body.classList.toggle("dark");
   body.classList.toggle("light");
-
   toggleBtn.textContent = body.classList.contains("dark") ? "🌙" : "☀️";
 });
 
-/* Ripple effect for taps */
-document.querySelectorAll(".nav-item").forEach(item => {
-  item.addEventListener("click", e => {
+/* Ripple Effect */
+document.querySelectorAll(".subject-card").forEach(card => {
+  card.addEventListener("click", e => {
     const ripple = document.createElement("span");
     ripple.style.position = "absolute";
-    ripple.style.background = "rgba(255,255,255,0.4)";
+    ripple.style.width = ripple.style.height = "150px";
     ripple.style.borderRadius = "50%";
-    ripple.style.width = ripple.style.height = "120px";
-    ripple.style.left = `${e.offsetX - 60}px`;
-    ripple.style.top = `${e.offsetY - 60}px`;
+    ripple.style.background = "rgba(255,255,255,0.4)";
+    ripple.style.left = `${e.offsetX - 75}px`;
+    ripple.style.top = `${e.offsetY - 75}px`;
     ripple.style.animation = "ripple 0.6s linear";
-    item.appendChild(ripple);
-
+    card.appendChild(ripple);
     setTimeout(() => ripple.remove(), 600);
   });
 });
